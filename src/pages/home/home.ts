a75bd229c5c8ddacc4bbd020a10eb242ec9f6ab1
Runion, Beau <Beau.Runion@flooranddecor.com>
2018-02-10 18:29:25 -0500

    added summary component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { SummaryPage } from '../summary/summary';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  user: string;
+  password: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+  }
+
+  login(){
+    let loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+
+    loader.present();
+
+    setTimeout(() => {
+      loader.dismiss();
+      this.navCtrl.setRoot(SummaryPage);
+    }, 3000);
 
   }
 
